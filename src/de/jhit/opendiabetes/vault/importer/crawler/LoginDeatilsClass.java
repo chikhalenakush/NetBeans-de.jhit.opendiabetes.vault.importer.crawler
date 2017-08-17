26487c08e2d16b5218c6a12475cb3fc3332453f4
chikhalenakush <chikhaleankush@gmail.com>
2017-08-17 08:40:30 +0200

    Update LoginDeatilsClass.java

--- src/de/jhit/opendiabetes/vault/importer/crawler/LoginDeatilsClass.java
+++ src/de/jhit/opendiabetes/vault/importer/crawler/LoginDeatilsClass.java
@@ -8,43 +8,42 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 public class LoginDeatilsClass {
-	private Map<String, String> loginCookies = new HashMap<String, String>();
-	public static String USERAGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
 
-	public Map<String, String> getcookies() {
-		return this.loginCookies;
+    private Map<String, String> loginCookies = new HashMap<String, String>();
+    public static String USERAGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
 
-	}
-	
-	public Boolean checkConnection(String username, String password, Logger logger) {
-		// check if login credentials are correct or nots
-		try {
-			logger.info("Inside class checkConnection");
+    public Map<String, String> getcookies() {
+        return this.loginCookies;
 
-			Connection.Response res = Jsoup.connect("https://carelink.minimed.eu/patient/j_security_check")
-					.data("j_username", username).data("j_password", password).method(Connection.Method.POST).execute();
-			loginCookies = res.cookies();
-			System.out.println("correct Username and Password");
-			logger.info("correct Username and Password");
-			return true;
-		} catch (Exception e) {
-			logger.info("Incorrect Username or Password");
-			System.out.println("Incorrect Username or Password");
-			return false;
-		}
+    }
 
-	}
+    public Boolean checkConnection(String username, String password, Logger logger) {
+        // check if login credentials are correct or nots
+        try {
+            logger.info("Inside class checkConnection");
 
-	
+            Connection.Response res = Jsoup.connect("https://carelink.minimed.eu/patient/j_security_check")
+                    .data("j_username", username).data("j_password", password).method(Connection.Method.POST).execute();
+            loginCookies = res.cookies();
+            System.out.println("correct Username and Password");
+            logger.info("correct Username and Password");
+            return true;
+        } catch (Exception e) {
+            logger.info("Incorrect Username or Password");
+            System.out.println("Incorrect Username or Password");
+            return false;
+        }
 
-	public String GetLanguage() {
-		// TODO Auto-generated method stub
-		
-		if(loginCookies.toString().contains("locale=de")){
-			return "de";
-		}else if(loginCookies.toString().contains("locale=en")){
-			return "en";
-		}
-		return null;
-	}
+    }
+
+    public String GetLanguage() {
+        // TODO Auto-generated method stub
+
+        if (loginCookies.toString().contains("locale=de")) {
+            return "de";
+        } else if (loginCookies.toString().contains("locale=en")) {
+            return "en";
+        }
+        return null;
+    }
 }
