cf3559e82a602a5a284ce05f8d51f21f57280b33
chikhalenakush <chikhaleankush@gmail.com>
2017-08-17 08:35:28 +0200

    Update TestLoginDetails.java

--- src/UnitTestCases/TestLoginDetails.java
+++ src/UnitTestCases/TestLoginDetails.java
@@ -10,18 +10,17 @@ import org.junit.runner.notification.Failure;
 
 public class TestLoginDetails {
 
-public void isLoginCorrect() throws IOException, ParseException {
-	// TODO Auto-generated method stub
+    public void isLoginCorrect() throws IOException, ParseException {
+        // TODO Auto-generated method stub
 
-	
-	//Result result = JUnitCore.this.run(jt);
-	Result result = JUnitCore.runClasses(JunitIsloginCorrect.class);
-	//Result result1 = JUnitCore.runClasses()
-	for (Failure failure : result.getFailures()) {
-		System.out.println(failure.toString());
-	}
+        //Result result = JUnitCore.this.run(jt);
+        Result result = JUnitCore.runClasses(JunitIsloginCorrect.class);
+        //Result result1 = JUnitCore.runClasses()
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
 
-	System.out.println("Test case Result is : " + result.wasSuccessful());
-	
-}
+        System.out.println("Test case Result is : " + result.wasSuccessful());
+
+    }
 }
