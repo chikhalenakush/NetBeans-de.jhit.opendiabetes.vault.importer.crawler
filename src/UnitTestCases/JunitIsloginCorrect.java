e7339a765847e803ee23bb326cbdeced4a88fa39
chikhalenakush <chikhaleankush@gmail.com>
2017-08-17 08:33:36 +0200

    Update JunitIsloginCorrect.java

--- src/UnitTestCases/JunitIsloginCorrect.java
+++ src/UnitTestCases/JunitIsloginCorrect.java
@@ -22,81 +22,77 @@ import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
 public class JunitIsloginCorrect {
-	public static String UserName;
-	public static String Password;
-	Boolean expected = false;
-	private static Scanner keyboard;
-	static Boolean UserInputTrueorFalse = false;
-	private Map<String, String> loginCookies = new HashMap<String, String>();
-	public static String UDERAGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
 
-	@Parameterized.Parameters
-	public static Collection Login() {
-		System.out.println("Inside Test");
-		keyboard = new Scanner(System.in);
-		System.out.println("Enter UserName");
-		String userNameFromUser = keyboard.next();
-		String passwordFromUser = null;
-		Console c = System.console();
-		if (c == null) {
-			System.err.println("No console.");
+    public static String UserName;
+    public static String Password;
+    Boolean expected = false;
+    private static Scanner keyboard;
+    static Boolean UserInputTrueorFalse = false;
+    private Map<String, String> loginCookies = new HashMap<String, String>();
+    public static String UDERAGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
 
-			final JPasswordField pf = new JPasswordField();
-			passwordFromUser = JOptionPane.showConfirmDialog(null, pf, "Password", JOptionPane.OK_CANCEL_OPTION,
-					JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION ? new String(pf.getPassword()) : "";
-		} else {
+    @Parameterized.Parameters
+    public static Collection Login() {
+        System.out.println("Inside Test");
+        keyboard = new Scanner(System.in);
+        System.out.println("Enter UserName");
+        String userNameFromUser = keyboard.next();
+        String passwordFromUser = null;
+        Console c = System.console();
+        if (c == null) {
+            System.err.println("No console.");
 
-			System.out.println("Enter Passowrd");
-			passwordFromUser = new String(c.readPassword("Password: "));
-		}
+            final JPasswordField pf = new JPasswordField();
+            passwordFromUser = JOptionPane.showConfirmDialog(null, pf, "Password", JOptionPane.OK_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION ? new String(pf.getPassword()) : "";
+        } else {
 
-		try {
-			System.out.println("Is the login details correct? \n Type true for yes \n Type false for no");
+            System.out.println("Enter Passowrd");
+            passwordFromUser = new String(c.readPassword("Password: "));
+        }
 
-			UserInputTrueorFalse = keyboard.nextBoolean();
-		}
+        try {
+            System.out.println("Is the login details correct? \n Type true for yes \n Type false for no");
 
-		catch (Exception e) {
-			System.out.println("Entered Value is not Boolean");
-			Login();
+            UserInputTrueorFalse = keyboard.nextBoolean();
+        } catch (Exception e) {
+            System.out.println("Entered Value is not Boolean");
+            Login();
 
-		}
+        }
 
-		return Arrays.asList(new Object[][] {
+        return Arrays.asList(new Object[][]{
+            {userNameFromUser, passwordFromUser, UserInputTrueorFalse},});
+    }
 
-				{ userNameFromUser, passwordFromUser, UserInputTrueorFalse },
+    public JunitIsloginCorrect(String user, String pass, Boolean expected) {
+        System.out.println("TestJunit-> UserName:" + user + " expected:" + expected);
+        this.UserName = user;
+        this.Password = pass;
+        this.expected = expected;
+    }
 
-		});
-	}
+    @Test
+    public void test() throws IOException, ParseException {
 
-	public JunitIsloginCorrect(String user, String pass, Boolean expected) {
-		System.out.println("TestJunit-> UserName:" + user + " expected:" + expected);
-		this.UserName = user;
-		this.Password = pass;
-		this.expected = expected;
-	}
+        Boolean Result = getLogin(UserName, Password);
 
-	@Test
-	public void test() throws IOException, ParseException {
+        assertEquals(expected, Result);
 
-		Boolean Result = getLogin(UserName, Password);
+    }
 
-		assertEquals(expected, Result);
+    private Boolean getLogin(String UserName, String Password) {
+        // TODO Auto-generated method stub
 
-	}
+        try {
 
-	private Boolean getLogin(String UserName, String Password) {
-		// TODO Auto-generated method stub
-
-		try {
-
-			Connection.Response res = Jsoup.connect("https://carelink.minimed.eu/patient/j_security_check")
-					.data("j_username", UserName).data("j_password", Password).method(Connection.Method.POST).execute();
-			return true;
-		} catch (Exception e) {
-			System.out.println("Incorrect Username or Password");
-			return false;
-		}
-	}
+            Connection.Response res = Jsoup.connect("https://carelink.minimed.eu/patient/j_security_check")
+                    .data("j_username", UserName).data("j_password", Password).method(Connection.Method.POST).execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Incorrect Username or Password");
+            return false;
+        }
+    }
 
 }
