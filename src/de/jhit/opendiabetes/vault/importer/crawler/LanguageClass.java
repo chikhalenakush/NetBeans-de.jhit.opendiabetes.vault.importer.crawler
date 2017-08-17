f8d38f6b45ac3cd472e470b0c1c541d1fd107a89
chikhalenakush <chikhaleankush@gmail.com>
2017-08-17 08:40:05 +0200

    Update LanguageClass.java

--- src/de/jhit/opendiabetes/vault/importer/crawler/LanguageClass.java
+++ src/de/jhit/opendiabetes/vault/importer/crawler/LanguageClass.java
@@ -6,19 +6,18 @@ import java.awt.event.KeyEvent;
 
 public class LanguageClass {
 
-	public int getReplacment(String lang, Logger logger) {
-		// This function is used when pressing keys.
-		logger.info("Inside Class, LanguageClass");
-		if(lang.contains("en")){
-			logger.info("Inside Class LanguageClass, English language");
-			return KeyEvent.VK_N;
-		} else 
-		if(lang.contains("de")){
-			logger.info("Inside Class LanguageClass, Deutsch language");
-			return KeyEvent.VK_W;
-		}else{		
-		return 0;
-		}
-	}
+    public int getReplacment(String lang, Logger logger) {
+        // This function is used when pressing keys.
+        logger.info("Inside Class, LanguageClass");
+        if (lang.contains("en")) {
+            logger.info("Inside Class LanguageClass, English language");
+            return KeyEvent.VK_N;
+        } else if (lang.contains("de")) {
+            logger.info("Inside Class LanguageClass, Deutsch language");
+            return KeyEvent.VK_W;
+        } else {
+            return 0;
+        }
+    }
 
 }
